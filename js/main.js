d84657b539f77ffa1383f46ee44a5e97b016e1a5
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-10 21:28:03 -0800

    listen for click event for the home icon & bottom button #3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 
+var $startButton = document.querySelector('.get-started-button');
+var $homePage = document.querySelector('.home-page');
 var $mainPage = document.querySelector('.main-page');
 var $activityPage = document.querySelector('.activity-page');
 var $conchButton = document.querySelector('.conch-button');
+var $activityList = document.querySelector('.activity-row');
+var $homeIcon = document.querySelectorAll('.go-home');
+var $searchIcon = document.querySelectorAll('.go-search');
+
+$startButton.addEventListener('click', function (event) {
+  $homePage.className = 'home-page hidden';
+  $mainPage.className = 'main-page';
+  $activityPage.className = 'activity-page hidden';
+
+});
 
 $conchButton.addEventListener('click', function (event) {
+  $homePage.className = 'home-page hidden';
   $mainPage.className = 'main-page hidden';
   $activityPage.className = 'activity-page';
+  getBoredData('');
+  getBoredData('');
+  getBoredData('');
+});
+
+document.addEventListener('click', function (event) {
+  if (event.target === $searchIcon[1]) {
+    $homePage.className = 'home-page hidden';
+    $mainPage.className = 'main-page';
+    $activityPage.className = 'activity-page hidden';
+  }
+  if (event.target === $homeIcon[0]) {
+    $homePage.className = 'home-page';
+    $mainPage.className = 'main-page hidden';
+    $activityPage.className = 'activity-page hidden';
+  }
+  if (event.target === $homeIcon[1]) {
+    $homePage.className = 'home-page';
+    $mainPage.className = 'main-page hidden';
+    $activityPage.className = 'activity-page hidden';
+  }
+
 });
 
 function getBoredData(name) {
@@ -19,31 +54,39 @@ function getBoredData(name) {
 }
 
 function renderingActivities(model) {
-  var hrElement = document.createElement('hr');
+  var divElement = document.createElement('div');
 
   // Title
   var activityTitle = document.createElement('h3');
   activityTitle.textContent = model.activity;
-  hrElement.appendChild(activityTitle);
+  divElement.appendChild(activityTitle);
 
   // Difficulty Level
   var difficultyLevel = document.createElement('p');
+  difficultyLevel.className = 'activity-description';
   difficultyLevel.textContent = 'Difficulty: ';
   difficultyLevel.appendChild(circleLevel(model.accessibility));
   activityTitle.appendChild(difficultyLevel);
 
   // Category
   var category = document.createElement('p');
+  category.className = 'activity-description';
   category.textContent = 'Category: ' + model.type;
   difficultyLevel.appendChild(category);
 
   // Participants
   var participants = document.createElement('p');
-  participants.textContent = 'Participants ';
+  participants.className = 'activity-description';
+  participants.textContent = 'Participants needed: ';
   participants.appendChild(numOfParticipants(model.participants));
   category.appendChild(participants);
 
-  return hrElement;
+  var hrElement = document.createElement('hr');
+  divElement.appendChild(hrElement);
+
+  $activityList.appendChild(divElement);
+
+  return divElement;
 }
 
 function circleLevel(decimal) {
@@ -51,12 +94,12 @@ function circleLevel(decimal) {
   var number = decimal * 10;
   for (var i = 0; i < number; i++) {
     var circleClosed = document.createElement('i');
-    circleClosed.className = 'fas fa-search search-icon';
+    circleClosed.className = 'fas fa-circle';
     circleSpan.appendChild(circleClosed);
   }
   for (var n = 10; n > number; n--) {
     var circleOpen = document.createElement('i');
-    circleOpen.className = 'far fa-search search-icon';
+    circleOpen.className = 'far fa-circle';
     circleSpan.appendChild(circleOpen);
   }
   return circleSpan;
